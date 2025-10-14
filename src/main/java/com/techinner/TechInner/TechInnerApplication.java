a4fe5bd5791aa620362d78325d6f95e267bf2a04
Pedro Ruan <pedroruan.ruan1@gmail.com>
2025-10-13 22:02:31 -0300

    Start the project

---
+++ src/main/java/com/techinner/TechInner/TechInnerApplication.java
@@ -0,0 +1,13 @@
+package com.techinner.TechInner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechInnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechInnerApplication.class, args);
+	}
+
+}
